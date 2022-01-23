078b21ff99f41dffafc2a6b4c8a522f48b02d5e0
lipiventura <diogofilipe@MBP-de-Diogo.Home>
2022-01-23 17:43:48 +0000

    commented and organized

--- ai.pl
+++ ai.pl
@@ -2,84 +2,14 @@
 %Para cada jogada avaliar com quantas jogadas possíveis o adversário fica (valor menor que 0) e o computador (valor maior que 0)
 %Somar os dois valores e optar pela jogada que retorne maior valor (heurística)
 
-checkAllAI(S,1,1,Value):- checkBoardAI(S,1,2,Value), checkBoardAI(S,2,1,Value).
-checkAllAI(S,1,8,Value):- checkBoardAI(S,1,7,Value), checkBoardAI(S,2,8,Value).
-checkAllAI(S,8,1,Value):- checkBoardAI(S,8,2,Value), checkBoardAI(S,7,1,Value).
-checkAllAI(S,8,8,Value):- checkBoardAI(S,8,7,Value), checkBoardAI(S,7,8,Value).
-
-checkAllAI(S,1,Column,Value):- Column2 is Column + 1, Column3 is Column - 1,
-                             checkBoardAI(S,2,Column,Value),checkBoardAI(S,1,Column2,Value),checkBoardAI(S,1,Column3,Value).
-checkAllAI(S,8,Column,Value):- Column2 is Column + 1, Column3 is Column - 1,
-                             checkBoardAI(S,7,Column,Value),checkBoardAI(S,8,Column2,Value),checkBoardAI(S,8,Column3,Value).
-checkAllAI(S,Row,1,Value):- Row2 is Row + 1, Row3 is Row - 1,
-                          checkBoardAI(S,Row,2,Value),checkBoardAI(S,Row2,1,Value),checkBoardAI(S,Row3,1,Value).
-checkAllAI(S,Row,8,Value):- Row2 is Row + 1, Row3 is Row - 1,
-                          checkBoardAI(S,Row,7,Value),checkBoardAI(S,Row2,8,Value),checkBoardAI(S,Row3,8,Value).
-                        
-checkAllAI(S,Row,Column,Value):- Row2 is Row + 1,Row3 is Row - 1,Column2 is Column + 1,Column3 is Column - 1,
-                               checkBoardAI(S,Row2,Column,Value),
-                               checkBoardAI(S,Row3,Column,Value),
-                               checkBoardAI(S,Row,Column2,Value),
-                               checkBoardAI(S,Row,Column3,Value).
-
-checkListAI([H|T],1,Value):- \+ H = Value.
-checkListAI([H|T],Column,Value):- Column > 1,
-                                     Column2 is Column - 1,
-                                     checkListAI(T,Column2,Value).
-
-checkBoardAI([H|T],1,Column,Value):- checkListAI(H,Column,Value).
-checkBoardAI([H|T],Row,Column,Value):- Row > 1,
-                                     Row2 is Row - 1,
-                                     checkBoardAI(T,Row2,Column,Value).
-
-
-checkAI(S,Row,Column,P1,P2,Row-Column):- masterRuleAI(S,Row,Column,P1,P2),
-                            checkBoardAI(S,Row,Column,P1),
-                            checkBoardAI(S,Row,Column,P2).
-
-masterRuleAI(S,Row,Column,P,P2):- checkAllAI(S,Row,Column,P2);
-                                \+ checkAllAI(S,Row,Column,P2), \+ checkAllAI(S,Row,Column,P).
-
-
-playableColumnAI(S,Row,9,P1,P2,L1,L1).
-playableColumnAI(S,Row,9,P1,P2,L1,L2):- playableColumnAI(S,Row,9,P1,P2,L1,L1).
-playableColumnAI(S,Row,Column,P1,P2,L1,L2):- check(S,Row,Column,P1,P2),
-                                     Column1 is Column + 1,
-                                     SubL = [Row,Column], append(L1,[SubL],L3),
-                                     playableColumnAI(S,Row,Column1,P1,P2,L3,L2);
-                                     \+ check(S,Row,Column,P1,P2),
-                                     Column1 is Column + 1,
-                                     playableColumnAI(S,Row,Column1,P1,P2,L1,L2).
-
-%obtém a lista de valid_moves para os 2 jogadores 
-valid_moves(S,L):- playableColumnAI(S,1,1,p1,p2,[],L2),
-                   playableColumnAI(S,2,1,p1,p2,L2,L3),
-                   playableColumnAI(S,3,1,p1,p2,L3,L4),
-                   playableColumnAI(S,4,1,p1,p2,L4,L5),
-                   playableColumnAI(S,5,1,p1,p2,L5,L6),
-                   playableColumnAI(S,6,1,p1,p2,L6,L7),
-                   playableColumnAI(S,7,1,p1,p2,L7,L8),
-                   playableColumnAI(S,8,1,p1,p2,L8,L9),
-                   LP1 = L9,
-                   playableColumnAI(S,1,1,p2,p1,[],M2),
-                   playableColumnAI(S,2,1,p2,p1,M2,M3),
-                   playableColumnAI(S,3,1,p2,p1,M3,M4),
-                   playableColumnAI(S,4,1,p2,p1,M4,M5),
-                   playableColumnAI(S,5,1,p2,p1,M5,M6),
-                   playableColumnAI(S,6,1,p2,p1,M6,M7),
-                   playableColumnAI(S,7,1,p2,p1,M7,M8),
-                   playableColumnAI(S,8,1,p2,p1,M8,M9),
-                   LP2 = M9,
-                   L = [LP1|LP2].
-
+%coloca a peça no sítio desejado (apenas para quando o computador joga)
 move(S,[H1|H2],SNew,P):- Row is H1, Column is H2, changeBoard(S,SNew,Row,Column,P).
 
 %invoca o predicado move e caso já tenho acabado, anuncia o fim do jogo
-handleList(S,SNew,Level,P,[]):- nl,write('               Game Over!                 '),nl, inverse(P,P2), game_over(S,P2),nl,play.
+handleList(S,SNew,Level,P,[]):- nl,write('               Game Over!                 '),nl, inverse(P,P2), game_over(S,P2),nl.
 handleList(S,SNew,1,P,[H|T]):- move(S,H,SNew,P). 
 
 %Nível 2 de AI
-%handleList(S,SNew,2,P,[]):- move(S,H,SNew,P). 
 handleList(S,SNew,2,P,L):- checkBestOption(S,SNew,L,P,64,M1).
 
 %obtém-se jogador adversário com base no que está a jogar
@@ -100,7 +30,7 @@ checkBestOption(S,SNew,[H|T],p2,N,M1):- move(S,H,S2,p2),
 
                              
 
-%vê qual é o maior e invoca checkBestOption de novo com o melhor movimento
+%vê qual é o maior e invoca checkBestOption de novo com o melhor movimento, comparando o número de jogadas que o advesário teria
 bestOptions(S,SNew,[],P,N,N2,M1,M2):- checkBestOption(S,SNew,[],P,64,M1).
 bestOptions(S,SNew,T,P,N,N2,M1,M2):- N > N2 ->
                                    checkBestOption(S,SNew,T,P,N2,M2);
--- state.pl
+++ state.pl
@@ -1,3 +1,4 @@
+%Estado inicial do jogo
 initial_state(S):- S = [[empty,empty,empty,empty,empty,empty,empty,empty],
         [empty,empty,empty,empty,empty,empty,empty,empty],
         [empty,empty,empty,empty,empty,empty,empty,empty],
@@ -7,6 +8,7 @@ initial_state(S):- S = [[empty,empty,empty,empty,empty,empty,empty,empty],
         [empty,empty,empty,empty,empty,empty,empty,empty],
         [empty,empty,empty,empty,empty,empty,empty,empty]].
 
+%Estado intermediário do jogo
 intermidiate_state(S):- S = [[p1,p1,p2,empty,empty,empty,empty,empty],
         [p1,p2,p2,empty,empty,empty,p2,empty],
         [empty,p2,p2,p2,empty,empty,p2,empty],
@@ -16,6 +18,7 @@ intermidiate_state(S):- S = [[p1,p1,p2,empty,empty,empty,empty,empty],
         [empty,p1,empty,empty,p1,empty,p1,empty],
         [p2,empty,empty,empty,empty,empty,empty,p2]].
 
+%Estado final do jogo
 final_state(S):- S = [[p1,p1,p2,p2,p2,empty,empty,empty],
         [p1,p2,p2,p2,p2,p2,p2,empty],
         [p2,p2,p2,p2,p2,p2,p2,empty],
@@ -25,110 +28,58 @@ final_state(S):- S = [[p1,p1,p2,p2,p2,empty,empty,empty],
         [empty,p1,p1,p1,p1,p1,p1,p2],
         [p2,p1,p1,p2,p1,p1,p1,p2]].
 
-
+%display do jogo player vs player
 display_game(S):-
         showTopBoard(S,1).
 
+%display do jogo player vs computer
 display_game_AI(S,Level):-
         showTopBoardOpt2(S,Level,1).
 
+%display do jogo computer vs computer
 display_game_only_AI(S,Level):-
         showTopBoardOpt3(S,Level,1).
 
 
+%parte de cima do jogo player vs player quando é a vez do jogador 1
 showTopBoard(S,1):- nl,
         write('           Player '), write(1), write(' Playing'),nl, 
         write('                                    '),nl,
         write('   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |'),nl,
         write('---|---|---|---|---|---|---|---|---|'),nl,
         showArcade(1,S),nl,
-        gameNotOver(S,p1,p2),
+        valid_moves(S,[H|T]),
+        handlePlayerList(S,P,H),       
         doChanging(S,SNew,p1,p2),
         showTopBoard(SNew,2).
 
+
+%parte de cima do jogo player vs player quando é a vez do jogador 2
 showTopBoard(S,2):- nl,
         write('           Player '), write(2), write(' Playing'),nl, 
         write('                                    '),nl,
         write('   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |'),nl,
         write('---|---|---|---|---|---|---|---|---|'),nl,
         showArcade(1,S),nl,
-        gameNotOver(S,p2,p1),
+        valid_moves(S,[H|T]),
+        handlePlayerList(S,P,T),
         doChanging(S,SNew,p2,p1),
         showTopBoard(SNew,1).
 
-showArcade(9,[]).
-showArcade(N,[H|T]):- alphabet(A,N),write(' '),write(A),write(' | '),
-                showLine(H),
-                nl,write('___|___|___|___|___|___|___|___|___|'),nl,
-                N1 is N + 1,
-                showArcade(N1,T).
 
-showLine([]).
-showLine([H|T]):- showChar(C,H),
-                write(C),write(' | '),
-                showLine(T).
-
-
-
-playerID(p1,1).
-playerID(p2,2).
-
-
-gameNotOver(S,P,P2):- playableColumn(S,1,1,P,P2,0,N2),
-                playableColumn(S,2,1,P,P2,0,N3),
-                playableColumn(S,3,1,P,P2,0,N4),
-                playableColumn(S,4,1,P,P2,0,N5),
-                playableColumn(S,5,1,P,P2,0,N6),
-                playableColumn(S,6,1,P,P2,0,N7),
-                playableColumn(S,7,1,P,P2,0,N8),
-                playableColumn(S,8,1,P,P2,0,N9),
-                N10 is N2 + N3, N11 is N10 + N4,
-                N12 is N11 + N5,N13 is N12 + N6,
-                N14 is N13 + N7,N15 is N14 + N8,
-                X is N15 + N9,handleN(S,P2,X).
-
-gameNotOverVSAI(S,P,P2):- playableColumn(S,1,1,P,P2,0,N2),
-                        playableColumn(S,2,1,P,P2,0,N3),
-                playableColumn(S,3,1,P,P2,0,N4),
-                playableColumn(S,4,1,P,P2,0,N5),
-                playableColumn(S,5,1,P,P2,0,N6),
-                playableColumn(S,6,1,P,P2,0,N7),
-                playableColumn(S,7,1,P,P2,0,N8),
-                playableColumn(S,8,1,P,P2,0,N9),
-                N10 is N2 + N3, N11 is N10 + N4,
-                N12 is N11 + N5,N13 is N12 + N6,
-                N14 is N13 + N7,N15 is N14 + N8,
-                X is N15 + N9,handleAI(S,P2,X).
-
-
-handleN(S,P,0):- nl,write('               Game Over!                 '),nl, game_over(S,P).
-handleN(S,P,X):- true.     
-
-game_over(S,P):- emptyRow(S,1,1,empty), nl,write('               It\'s a Tie!               '),nl,nl, returnToMenu.
-game_over(S,P):- nl,write('              Player '),playerID(P,N),write(N),write(' Won!             '),nl,nl, returnToMenu.
-game_over_AI(S,P):- nl,write('              Computer Won!             '),nl,nl, returnToMenu.
-
-
-
-doChanging(S,SNew,P,P2):- repeat,
-                rowSelection(Row),
-                columnSelection(Column),
-                masterRule(S,Row,Column,P,P2),
-                changeBoard(S,SNew,Row,Column,P).
-
-
-
+%parte de cima do jogo player vs computer quando é a vez do jogador 1
 showTopBoardOpt2(S,Level,1):- nl,
             write('           Player '), write(1), write(' Playing'),nl, 
             write('                                    '),nl,
             write('   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |'),nl,
             write('---|---|---|---|---|---|---|---|---|'),nl,
             showArcade(1,S),nl,
-            gameNotOver(S,p1,p2),
+            valid_moves(S,[H|T]),
+            handlePlayerList(S,P,H),
             doChanging(S,SNew,p1,p2),
             showTopBoardOpt2(SNew,Level,2).
 
-
+%parte de cima do jogo player vs computer quando é a vez do jogador 2
 showTopBoardOpt2(S,Level,2):- nl,
             write('           Computer Playing'),nl, 
             write('                                    '),nl,
@@ -140,7 +91,7 @@ showTopBoardOpt2(S,Level,2):- nl,
             canProceed(S),
             showTopBoardOpt2(SNew,Level,1).
 
-
+%parte de cima do jogo computer vs computer quando é a vez do jogador 1
 showTopBoardOpt3(S,Level,1):- nl,
             write('           Computer 1 Playing'),nl, 
             write('                                    '),nl,
@@ -152,6 +103,7 @@ showTopBoardOpt3(S,Level,1):- nl,
             canProceed(S),
             showTopBoardOpt3(SNew,Level,2).
 
+%parte de cima do jogo computer vs computer quando é a vez do jogador 2
 showTopBoardOpt3(S,Level,2):- nl,
             write('           Computer 2 Playing'),nl, 
             write('                                    '),nl,
@@ -162,3 +114,31 @@ showTopBoardOpt3(S,Level,2):- nl,
             handleList(S,SNew,Level,p2,T),
             canProceed(S),
             showTopBoardOpt3(SNew,Level,1).
+
+%mostra o resto do jogo (arcada)
+showArcade(9,[]).
+showArcade(N,[H|T]):- alphabet(A,N),write(' '),write(A),write(' | '),
+                showLine(H),
+                nl,write('___|___|___|___|___|___|___|___|___|'),nl,
+                N1 is N + 1,
+                showArcade(N1,T).
+
+%mostra cada linha e as peças nela
+showLine([]).
+showLine([H|T]):- showChar(C,H),
+                write(C),write(' | '),
+                showLine(T).
+
+%permite obter o jogador com base no ID ou o ID com base no jogador
+playerID(p1,1).
+playerID(p2,2).
+
+%deteta quando a lista de valid moves está vazia (e o jogo chegou ao fim assim sendo)
+handlePlayerList(S,P,[]):- nl,write('               Game Over!                 '),nl, inverse(P,P2), game_over(S,P2),nl.
+handlePlayerList(S,P,X).
+
+%final do jogo, diz quem ganhou e retorna ao menu
+game_over(S,P):- emptyRow(S,1,1,empty), nl,write('               It\'s a Tie!               '),nl,nl, returnToMenu.
+game_over(S,P):- nl,write('              Player '),playerID(P,N),write(N),write(' Won!             '),nl,nl, returnToMenu.
+game_over_AI(S,P):- nl,write('              Computer Won!             '),nl,nl, returnToMenu.
+
